575d7d6fe4fc6386774e527f67bc4957c4191f15
anandkarna-anu <anandkarnaanu@gmail.com>
2025-08-21 20:32:04 +0545

    made message for invalid ids(strings) by finding

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ app.get('/', (request, response) => {
   response.send('Hello, backend is running!');
 });
 
-let lists = [
+let persons = [
     { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -112,8 +112,15 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end();
 });
 
-app.get('/api/persons/5',(request,response)=> {
-  response.status(404).send(`looks like there is no data for the given id`)
+app.get('/api/persons/:id',(request,response)=> {
+  const id = request.params.id;
+const person = persons.find(person => person.id === id)
+if(person){
+  response.json(person)
+}
+else{
+  response.status(404).json({error:`given id ${id} has no data`})
+}
 })
 
 app.get('/api/persons', (request,response)=> {

b04ad7f94712da8baf30d54efa1756d7bc808608
anandkarna-anu <anandkarnaanu@gmail.com>
2025-08-21 13:45:24 +0545

    exercise 3.1 adding data for display , /api/persons and notes too

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('Hello, backend is running!');
+});
+
+let lists = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
 let notes = [
   {
     id: "1", // <-- Change to string
@@ -29,6 +57,10 @@ app.get('/api/notes/:id', (request, response) => {
     response.status(404).end(); // A simple .end() is common for 404
   }
 });
+ 
+app.get('/api/notes',(request,response) =>{
+  response.json(notes)
+})
 
 app.get('/api/notes/:id',(request,response)=> {
   const id = Number(request.params.id);
@@ -79,6 +111,10 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id); // "1" !== "3" works!
   response.status(204).end();
 });
+
+app.get('/api/persons', (request,response)=> {
+  response.json(lists)
+})
 
 const PORT= 3001
 app.listen(PORT, ()=>{

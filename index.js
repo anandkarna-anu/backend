e3235cc7c5ada844dd06ca9c05e37d285d755fa2
anandkarna-anu <anandkarnaanu@gmail.com>
2025-08-24 09:20:58 +0545

    making notes and persons array from string

--- index.js
+++ index.js
@@ -28,42 +28,42 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 let persons = [
     { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: "1",
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: "2",
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: "3",
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: "4",
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
 let notes = [
   {
-    "id": "1",
-    "content": "HTML is easy",
-    "important": true 
+    id: "1",
+    content: "HTML is easy",
+    important: true 
   },
   {
-    "id": "2",
-    "content": "Browser can execute only Javascript",
-    "important": false
+    id: "2",
+    content: "Browser can execute only Javascript",
+    important: false
   }, 
   {
-    "id": "3",
-    "content" : "GET and POST are the important methods of HTTP Protocol",
-    "important" : true
+    id: "3",
+    content : "GET and POST are the important methods of HTTP Protocol",
+    important : true
   }
 ]
 
